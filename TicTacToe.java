4a1b6da09a8d7faa561b66496a6854ea234013a9
Baran Usluel <me@baranusluel.com>
2018-05-07 17:11:19 -0400

    Add JavaFX for GUI

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,8 +3,27 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Arrays;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.FlowPane;
 
-public class TicTacToe {
+public class TicTacToe extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        FlowPane flow = new FlowPane();
+        flow.setVgap(10);
+        flow.setHgap(10);
+        flow.setPrefWrapLength(300);
+
+        Scene scene = new Scene(flow);
+        stage.setScene(scene);
+        stage.setTitle("Tic-Tac-Toe");
+        stage.setWidth(300);
+        stage.setHeight(300);
+        stage.show();
+    }
 
     //human vs human
     private static void multiplayer() {
@@ -39,7 +58,7 @@ public class TicTacToe {
     }
 
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         multiplayer();
-    }
+    }*/
 }
